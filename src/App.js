21f245e01c46f240954d7bd8738a51f0fbd1a36e
Matt Bendel <mattbendel@Matts-Mac-mini.columbus.rr.com>
2021-02-17 13:50:37 -0500

    Began web page architecture design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React from 'react'
+
 import logo from './logo.svg';
 import './App.css';
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import DeviceLink from "./components/DeviceLink";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authenticated: false,
+      linked: false,
+    }
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            {this.state.authenticated ? this.state.linked ? (<Home />) : (<DeviceLink />) : (<SignUp />)}
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SignUp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasAccount: false,
+        }
+    }
+
+    render(){
+        return;
+    }
+}
