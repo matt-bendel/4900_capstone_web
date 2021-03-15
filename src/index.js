1c66e2944338690cdfc8817f1b7f182f11d5ab7b
Matt Bendel <mattbendel@Matts-Mac-mini.columbus.rr.com>
2021-03-15 19:52:35 -0400

    Design overhaul and proper skeleton - needs refining of info and firebase integration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import AdminLayout from "layouts/Admin/Admin.js";
+
+import "assets/scss/black-dashboard-react.scss";
+import "assets/demo/demo.css";
+import "assets/css/nucleo-icons.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeContextWrapper>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" render={(props) => <AdminLayout {...props} />} />
+          <Redirect from="/" to="/dashboard" />
+        </Switch>
+      </BrowserRouter>
+  </ThemeContextWrapper>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/views/Dashboard.js
@@ -0,0 +1,116 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+
+// reactstrap components
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Label,
+  FormGroup,
+  Input,
+  Table,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+// core components
+import {
+  chartExample1,
+  chartExample2,
+  chartExample3,
+  chartExample4,
+} from "variables/charts.js";
+
+function Dashboard(props) {
+  const [bigChartData, setbigChartData] = React.useState("data1");
+  const setBgChartData = (name) => {
+    setbigChartData(name);
+  };
+  return (
+    <>
+      <div className="content">
+        <h1>Your device last cleaned the door handle at TEMP.</h1>
+        <Row>
+          <Col lg="6">
+            <Card className="card-chart">
+              <CardHeader>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-button-power text-info" /> Battery Life
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6">
+            <Card className="card-chart">
+              <CardHeader>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-bag-16 text-info" />{" "}
+                  Remaining Disinfectant
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <CardTitle tag="h2">
+                      <i className="tim-icons icon-chart-bar-32 text-info" />{" "}
+                      Average Daily Touches
+                    </CardTitle>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1[bigChartData]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
