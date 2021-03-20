0f115482d40d9c8f00b63aa4f613a7b1a2a98b72
Matt Bendel <mattbendel@Matts-Mac-mini.columbus.rr.com>
2021-03-20 14:42:51 -0400

    ?

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -63,7 +63,6 @@ function Dashboard(props) {
   let user = React.useContext(UserContext).user;
 
   React.useEffect(() => {
-    firebase.firestore().collection('devices').doc('12345').update({battery: false}).catch((e) => console.log(e));
     let deviceRef = firebase.firestore().collection('devices').doc(user.deviceId);
     const listener = deviceRef.onSnapshot((doc) => {
       if (batteryPercent !== doc.data().percentBattery && batteryPercent !== "") {
